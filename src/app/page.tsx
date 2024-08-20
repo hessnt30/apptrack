94d16d3e22a0d14354d810d3fbfda60bd8131a32
hessnt30 <hessnt30@vt.edu>
2024-08-19 21:32:08 -0400

    removed comments containing confidential info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,6 @@ import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-// Supabase password: Wd2mWzxU9LYW14if
-
-// Public API Key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kcm9kdnJqbHB3ZnN4ZXZxa3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM3MzQ1NjAsImV4cCI6MjAzOTMxMDU2MH0.hF2J_O_VpVdzGFXkBiOWvSLX3AQN4CmY-aIDOfmlYJg
-
-// Row Level Secret: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kcm9kdnJqbHB3ZnN4ZXZxa3dtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMzczNDU2MCwiZXhwIjoyMDM5MzEwNTYwfQ.1pBDUxqHdYtHVRC75P_hR3Sbz-HGOyP8bKyptXMobzg
-
 export default function Home() {
   const { user } = useAuth();
   const router = useRouter();
